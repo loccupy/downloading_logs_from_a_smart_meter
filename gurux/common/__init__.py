9a2f0734ed77acbfa5dd3568bb700dfff832c53d
nikitinas <as.nikitin@promenergo-rt.ru>
2025-07-31 13:54:13 +0300

    иниты туда-сюда

--- gurux/common/__init__.py
+++
@@ -1,7 +0,0 @@
-from .GXCommon import GXCommon
-from .IGXMedia import IGXMedia
-from .IGXMediaListener import IGXMediaListener
-from .TraceEventArgs import TraceEventArgs
-from .ReceiveEventArgs import ReceiveEventArgs
-from .ReceiveParameters import ReceiveParameters
-from .TimeoutException import TimeoutException
---
+++ libs/gurux/common/__init__.py
@@ -0,0 +1,7 @@
+from .GXCommon import GXCommon
+from .IGXMedia import IGXMedia
+from .IGXMediaListener import IGXMediaListener
+from .TraceEventArgs import TraceEventArgs
+from .ReceiveEventArgs import ReceiveEventArgs
+from .ReceiveParameters import ReceiveParameters
+from .TimeoutException import TimeoutException

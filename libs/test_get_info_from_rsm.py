48aae0f669125884c418a8e3fc324e618c6f4185
nikitinas <as.nikitin@promenergo-rt.ru>
2025-12-03 17:17:54 +0300

    Доработка

--- libs/test_get_info_from_rsm.py
+++ libs/test_get_info_from_rsm.py
@@ -5,14 +5,16 @@ headers = {
     'Content-Type': 'application/json; charset=utf-8'
 }
 
+base_url = 'https://api.rsm.promenergo.local'
+
 
 def get_token():
-    url = 'https://api.rsm.promenergo.local/api/Auth/Login'
+    ext = '/api/Auth/Login'
     body = {
         "id": "1003",
         "password": "123"
     }
-    response = requests.post(url, json=body, headers=headers, verify=False)
+    response = requests.post(base_url + ext, json=body, headers=headers, verify=False)
 
     token = response.cookies
 
@@ -20,10 +22,9 @@ def get_token():
 
 
 def get_stand(cookies):
-    base = 'https://api.rsm.promenergo.local'
     ext = '/api/Stand/Get'
     response = requests.get(
-        base + ext,
+        base_url + ext,
         verify=False, headers=headers, cookies=cookies)
     res = {}
     for i in response.json()['value']:
@@ -32,17 +33,24 @@ def get_stand(cookies):
 
 
 @pytest.mark.asd
-def test_123():
+def test_get_serial_number_from_stand():
     cookies = get_token()
 
     stands = get_stand(cookies)
+    print(stands)
 
-    response = requests.get(f'https://api.rsm.promenergo.local/api/Cell/Get/ByStandId/{stands['1Ф Стенд ЛП']}',
+    id_stand = stands['1Ф Стенд ЛП']
+
+    response = requests.get(f'https://api.rsm.promenergo.local/api/Cell/Get/ByStandId/{id_stand}',
                             verify=False, headers=headers, cookies=cookies)
 
     assert response.status_code == 200
+    # print(response.json()['value'])
+    # print(response.json()['value'][0]['meter']['serialNumber'])
 
-    res = [''.join(i['meter']['serialNumber'][-4:]) for i in response.json()['value']]
-
+    # res = [''.join(i['meter']) for i in response.json()['value']]
+    data = response.json()['value']
+    res = [i['meter']['serialNumber'][-4:] for i in data if 'meter' in i]
+    #
     res = [num.lstrip('0') or '0' for num in res]
     print(res)

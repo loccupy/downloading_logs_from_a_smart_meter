ee879fc1ba3e20c8ac0508b110e98c44fca531e2
nikitinas <as.nikitin@promenergo-rt.ru>
2025-12-29 12:07:03 +0300

    Revert "В журнале самодиагностики добавил новые коды: 130/131/134/135/136 В профилях энергии 1 и 2 добавил проверку интервалов времени 30/60 минут"
    This reverts commit 11d04ad1

--- libs/log_analysis.py
+++ libs/log_analysis.py
@@ -7,7 +7,6 @@ from openpyxl.reader.excel import load_workbook
 from openpyxl.styles import PatternFill
 
 from libs.Utils import is_valid_date_for_anal, parse_log_name
-from libs.sending_message import add_to_global_list
 
 pink_fill = PatternFill(start_color='FFCC99FF',
                         end_color='FFCC99FF',
@@ -49,7 +48,6 @@ def analysis_correct_date_ref(sheets, log_name):
                 cell.fill = pink_fill
                 sleep(0.1)
                 print(f"Невалидная дата в строчке {cell.row} в '{parse_log_name(log_name)}'")
-                add_to_global_list(f"Невалидная дата в строчке {cell.row} в '{parse_log_name(log_name)}'")
     except Exception as e:
         raise f"Ошибка {e} при анализе 'Время фиксации записи' на последовательность в {parse_log_name(log_name)}"
 
@@ -89,8 +87,6 @@ def time_ordering_analysis_ref(sheets, log_name):
                     print(
                         f"Некорректная последовательность фиксации записи в строчках {cell_0.row}-{cell_1.row} в"
                         f" '{parse_log_name(log_name)}'")
-                    add_to_global_list(f"Некорректная последовательность фиксации записи в строчках {cell_0.row}-{cell_1.row} в"
-                        f" '{parse_log_name(log_name)}'")
             cell_0 = row[0]
     except Exception as e:
         raise f"Ошибка {e} при анализе 'Время фиксации записи' на валидность в {parse_log_name(log_name)}"
@@ -122,8 +118,6 @@ def ipu_working_hours_ref(sheets, log_name):
                 sleep(0.1)
                 print(f"Некорректный тип данных 'Время работы ПУ' в ячейке {cell_1.row} в"
                       f" '{parse_log_name(log_name)}'")
-                add_to_global_list(f"Некорректный тип данных 'Время работы ПУ' в ячейке {cell_1.row} в"
-                      f" '{parse_log_name(log_name)}'")
                 continue
             else:
                 res = int(cell_1.value) - int(cell_0.value)
@@ -136,8 +130,6 @@ def ipu_working_hours_ref(sheets, log_name):
                     sleep(0.1)
 
                     print(f"Некорректная последовательность 'Время работы ПУ' в строчках {cell_0.row}-{cell_1.row} в"
-                          f" '{log_name.replace(" ", "е ", 1)}'")
-                    add_to_global_list(f"Некорректная последовательность 'Время работы ПУ' в строчках {cell_0.row}-{cell_1.row} в"
                           f" '{log_name.replace(" ", "е ", 1)}'")
 
                 cell_0 = row[0]
@@ -182,8 +174,6 @@ def time_ordering_analysis_for_daily_profile(sheets, log_name):
                     print(
                         f"Некорректная последовательность фиксации записи в строчках {cell_0.row}-{cell_1.row} в"
                         f" '{parse_log_name(log_name)}'")
-                    add_to_global_list(f"Некорректная последовательность фиксации записи в строчках {cell_0.row}-{cell_1.row} в"
-                      f" '{parse_log_name(log_name)}'")
             cell_0 = row[0]
     except Exception as e:
         raise f"Ошибка {e} при анализе 'Время фиксации записи' в {parse_log_name(log_name)}"
@@ -226,9 +216,6 @@ def time_ordering_analysis_for_month_profile(sheets, log_name):
                     print(
                         f"Некорректная последовательность фиксации записи в строчках {cell_0.row}-{cell_1.row} в"
                         f" '{parse_log_name(log_name)}'")
-                    add_to_global_list(
-                        f"Некорректная последовательность фиксации записи в строчках {cell_0.row}-{cell_1.row} в"
-                        f" '{parse_log_name(log_name)}'")
             cell_0 = row[0]
     except Exception as e:
         raise f"Ошибка {e} при анализе 'Время фиксации записи' в {parse_log_name(log_name)}"
@@ -267,133 +254,6 @@ def checkForSelfDiagnostics(sheets, log_name):
                 print(
                     f"Код ошибки {row[0].value.split(',')[0]} в строчке {row[0].row} в"
                     f" '{parse_log_name(log_name)}'")
-                add_to_global_list(
-                    f"Код ошибки {row[0].value.split(',')[0]} в строчке {row[0].row} в"
-                    f" '{parse_log_name(log_name)}'")
-
-    except Exception as e:
-        raise f"Ошибка {e} при анализе кодов ошибок в {parse_log_name(log_name)}"
-
-
-# проверяет повторные включения или выключения в журнале вкл\выкл
-def checking_for_repeated_on_or_offs(sheets, log_name):
-    format_dt = "%d.%m.%y %H:%M:%S"
-    try:
-        sheet = sheets[log_name]
-        column = None
-        column_names = [cell for cell in sheet[1]]
-        # Получаем номер столбца по названию
-        for i in column_names:
-            if i.value == 'Событие':
-                column = i.column
-                break
-
-        # Проверяем все значения столбца и, в случае совпадения условия, перекрашиваем ячейку
-        for i, row in enumerate(sheet.iter_rows(min_row=2, min_col=column, max_col=column)):
-            # Получаем и сохраняем первую ячейку + пропускаем ее для обработки
-            if i == 0:
-                cell_0 = row[0]
-                continue
-
-            # Получаем ячейку
-            cell_1 = row[0]
-
-            if str(cell_0.value) == str(cell_1.value):
-                # Проверяем условие
-
-                cell_0.fill = pink_fill
-                sleep(0.1)
-                cell_1.fill = pink_fill
-                sleep(0.1)
-
-                print(
-                    f"Повторяется код события в строчках {cell_0.row}-{cell_1.row} в"
-                    f" '{parse_log_name(log_name)}'")
-                add_to_global_list(f"Повторяется код события в строчках {cell_0.row}-{cell_1.row} в"
-                    f" '{parse_log_name(log_name)}'")
-            cell_0 = row[0]
-    except Exception as e:
-        raise f"Ошибка {e} при анализе повторных кодов событий в {parse_log_name(log_name)}"
-
-
-# проверяет очередность валидных дат в столбце "Время фиксации записи" в срезе мгновенных значений
-def time_ordering_analysis_for_artur_profile(sheets, log_name):
-    format_dt = "%d.%m.%y %H:%M:%S"
-    try:
-        sheet = sheets[log_name]
-        column = None
-        column_names = [cell for cell in sheet[1]]
-        # Получаем номер столбца по названию
-        for i in column_names:
-            if i.value == 'Время фиксации записи':
-                column = i.column
-                break
-
-        # Проверяем все значения столбца и, в случае совпадения условия, перекрашиваем ячейку
-        for i, row in enumerate(sheet.iter_rows(min_row=2, min_col=column, max_col=column)):
-            # Получаем и сохраняем первую ячейку + пропускаем ее для обработки
-            if i == 0:
-                cell_0 = row[0]
-                continue
-
-            # Получаем ячейку
-            cell_1 = row[0]
-
-            if is_valid_date_for_anal(str(cell_0.value)) and is_valid_date_for_anal(str(cell_1.value)):
-                delta = (datetime.datetime.strptime(str(cell_1.value), format_dt) -
-                         datetime.datetime.strptime(str(cell_0.value), format_dt))
-                # Проверяем условие
-                if delta != timedelta(minutes=30):
-                    cell_0.fill = pink_fill
-                    sleep(0.1)
-                    cell_1.fill = pink_fill
-                    sleep(0.1)
-
-                    print(
-                        f"Некорректная разница во времени фиксации записей в строчках {cell_0.row}-{cell_1.row} в"
-                        f" '{parse_log_name(log_name)}'")
-            cell_0 = row[0]
-    except Exception as e:
-        raise f"Ошибка {e} при анализе 'Время фиксации записи' в {parse_log_name(log_name)}"
-        raise f"Ошибка {e} при анализе 'Время фиксации записи' в {parse_log_name(log_name)}"
-
-
-#Функция проверки ошибок самодиагностики
-def checkForSelfDiagnostics(sheets, log_name):
-    #Словарь ошибок самодиагностики
-    errorsList = ["2, Измерительный блок - ошибка",
-                  "4, Вычислительный блок - ошибка",
-                  "5, Часы реального времени - ошибка",
-                  "7, Блок питания - ошибка",
-                  "9, Дисплей - ошибка",
-                  "11, Блок памяти - ошибка",
-                  "13, Блок памяти программ - ошибка",
-                  "15, Система тактирования ядра - ошибка",
-                  "17, Система тактирования часов - ошибка",
-                  "129, Аппаратный сброс часов реального времени",
-                  "130, Программный сброс часов реального времени",
-                  "131, Сброс микроконтроллера при просадке напряжения",
-                  "132, Сброс микроконтроллера сторожевым таймером",
-                  "134", "135", "136"]
-
-    try:
-        sheet = sheets[log_name]
-        column = None
-        column_names = [cell for cell in sheet[1]]
-        # Получаем номер столбца по названию
-        for i in column_names:
-            if i.value == "Событие":
-                column = i.column
-                break
-
-        # Проверяем все значения столбца и, в случае совпадения условия, перекрашиваем ячейку
-        for row in sheet.iter_rows(min_row=2, min_col=column, max_col=column):
-            if row[0].value in errorsList:
-                row[0].fill = pink_fill
-                sleep(0.1)
-                print(
-                    f"Код ошибки {row[0].value.split(',')[0]} в строчке {row[0].row} в"
-                    f" '{parse_log_name(log_name)}'")
 
     except Exception as e:
         raise f"Ошибка {e} при анализе кодов ошибок в {parse_log_name(log_name)}"
@@ -466,88 +326,6 @@ def time_ordering_analysis_for_artur_profile(sheets, log_name):
                          datetime.datetime.strptime(str(cell_0.value), format_dt))
                 # Проверяем условие
                 if delta != timedelta(minutes=30):
-                    cell_0.fill = pink_fill
-                    sleep(0.1)
-                    cell_1.fill = pink_fill
-                    sleep(0.1)
-
-                    print(
-                        f"Некорректная разница во времени фиксации записей в строчках {cell_0.row}-{cell_1.row} в"
-                        f" '{parse_log_name(log_name)}'")
-            cell_0 = row[0]
-    except Exception as e:
-        raise f"Ошибка {e} при анализе 'Время фиксации записи' в {parse_log_name(log_name)}"
-
-
-# проверяет очередность валидных дат в столбце "Время фиксации записи" в профиле энергии за интервал 1
-def time_ordering_analysis_for_integration_interval_1(sheets, log_name):
-    format_dt = "%d.%m.%y %H:%M:%S"
-    try:
-        sheet = sheets[log_name]
-        column = None
-        column_names = [cell for cell in sheet[1]]
-        # Получаем номер столбца по названию
-        for i in column_names:
-            if i.value == 'Время фиксации записи':
-                column = i.column
-                break
-
-        # Проверяем все значения столбца и, в случае совпадения условия, перекрашиваем ячейку
-        for i, row in enumerate(sheet.iter_rows(min_row=2, min_col=column, max_col=column)):
-            # Получаем и сохраняем первую ячейку + пропускаем ее для обработки
-            if i == 0:
-                cell_0 = row[0]
-                continue
-
-            # Получаем ячейку
-            cell_1 = row[0]
-
-            if is_valid_date_for_anal(str(cell_0.value)) and is_valid_date_for_anal(str(cell_1.value)):
-                delta = (datetime.datetime.strptime(str(cell_1.value), format_dt) -
-                         datetime.datetime.strptime(str(cell_0.value), format_dt))
-                # Проверяем условие
-                if delta != timedelta(minutes=30):
-                    cell_0.fill = pink_fill
-                    sleep(0.1)
-                    cell_1.fill = pink_fill
-                    sleep(0.1)
-
-                    print(
-                        f"Некорректная разница во времени фиксации записей в строчках {cell_0.row}-{cell_1.row} в"
-                        f" '{parse_log_name(log_name)}'")
-            cell_0 = row[0]
-    except Exception as e:
-        raise f"Ошибка {e} при анализе 'Время фиксации записи' в {parse_log_name(log_name)}"
-
-
-# проверяет очередность валидных дат в столбце "Время фиксации записи" в профиле энергии за интервал 2
-def time_ordering_analysis_for_integration_interval_2(sheets, log_name):
-    format_dt = "%d.%m.%y %H:%M:%S"
-    try:
-        sheet = sheets[log_name]
-        column = None
-        column_names = [cell for cell in sheet[1]]
-        # Получаем номер столбца по названию
-        for i in column_names:
-            if i.value == 'Время фиксации записи':
-                column = i.column
-                break
-
-        # Проверяем все значения столбца и, в случае совпадения условия, перекрашиваем ячейку
-        for i, row in enumerate(sheet.iter_rows(min_row=2, min_col=column, max_col=column)):
-            # Получаем и сохраняем первую ячейку + пропускаем ее для обработки
-            if i == 0:
-                cell_0 = row[0]
-                continue
-
-            # Получаем ячейку
-            cell_1 = row[0]
-
-            if is_valid_date_for_anal(str(cell_0.value)) and is_valid_date_for_anal(str(cell_1.value)):
-                delta = (datetime.datetime.strptime(str(cell_1.value), format_dt) -
-                         datetime.datetime.strptime(str(cell_0.value), format_dt))
-                # Проверяем условие
-                if delta != timedelta(minutes=60):
                     cell_0.fill = pink_fill
                     sleep(0.1)
                     cell_1.fill = pink_fill
